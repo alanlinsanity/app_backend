d71aae75e5904bacd5c8ed422def614e83e05191
alanlinsanity <alan.linyh@gmail.com>
2022-04-09 11:26:07 +0800

    added to schema

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -7,40 +7,41 @@ router.get("/seed", async (req, res) => {
 
   const listings = [
     {
-      postal: 128017,
+      postal: "128017",
       district: 5,
       address: "91 West Coast Drive",
       property_type: "HDB",
-      likes: 5,
       size: 900,
       price: 3500,
       image: "https://www.urbanjourney.com/wp-content/uploads/2019/05/Screen-Shot-2019-05-20-at-3.11.48-PM.png",
       no_of_bedrooms: 3,
       no_of_bathrooms: 2,
+      description: "This is a well-furnished HDB that is just 500m away from Clementi MRT."
+
     },
     {
-      postal: 750335,
+      postal: "750335",
       district: 27,
       address: "335 Sembawang Close",
       property_type: "HDB",
-      likes: 3,
       size: 1100,
       price: 3200,
       image: "https://www.asiaone.com/sites/default/files/original_images/Sep2019/230919_hmlet8.jpg",
       no_of_bedrooms: 3,
       no_of_bathrooms: 2,
+      description: "HDB that is just 500m away from Sembawang MRT. Close to market and eateries."
     },
     {
-        postal: 099786,
+        postal: "099786",
         district: 4,
         address: "14 Bukit Teresa Close",
         property_type: "Private",
-        likes: 2,
         size: 2000,
         price: 4500,
         image: "https://www.99.co/singapore/insider/wp-content/uploads/2021/07/DSC_3450-min-e1626319307277.jpg",
         no_of_bedrooms: 4,
         no_of_bathrooms: 3,
+        description: "This is a Private development that is close to the city. 5 minutes walk to bus-stop."
       }
   ]
   await Listing.deleteMany({});
--- models/Listing.js
+++ models/Listing.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const listingSchema = mongoose.Schema({
-  postal: { type: Number, required: true },
+  postal: { type: String, required: true },
   district: { type: Number, required: true },
   address: { type: String, required: true },
   property_type: { type: String, required: true},
-  likes: { type: Number, default: 0 },
   size: { type: Number, required: true },
   price: { type: Number, required: true},
   image: { type: String, required: true},
   no_of_bedrooms: { type: Number, required: true},
   no_of_bathrooms: { type: Number, required: true},
+  description: { type: String }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
