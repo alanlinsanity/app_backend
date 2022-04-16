fdb941afd3078aa95ba47b4eee9585e2baac6c30
Mork <mawwwrk@gmail.com>
2022-04-16 11:27:31 +0800

    feat: users model

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,8 @@
+const { hash, compare } = require("bcrypt");
 const { Schema, model } = require("mongoose");
+const generateAccessToken = require("../util/authentication");
+const jwt = require("jsonwebtoken");
 
-const bcrypt = require("bcrypt");
 const rounds = 10;
 
 const userSchema = new Schema({
@@ -21,12 +23,12 @@ const userSchema = new Schema({
 });
 
 userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
+  return compare(password, this.password);
 };
 
 userSchema.pre("save", function (next) {
   if (!this.isModified("password")) return next();
-  bcrypt.hash(this.password, rounds, function (err, hash) {
+  hash(this.password, rounds, function (err, hash) {
     if (err) return next(err);
     this.password = hash;
     next();
