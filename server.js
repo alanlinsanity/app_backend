9444ee919e2e2f7e63c9668f73d6e3e956e06091
Mork <mawwwrk@gmail.com>
2022-04-17 00:02:58 +0800

    feat: more auth

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const ListingController = require("./controllers/listingController");
 const AuthController = require("./controllers/auth");
+const morgan = require("morgan");
+const morganFmt = process.env.NODE_ENV === "development" ? "dev" : "tiny";
 
 const app = express();
 const PORT = process.env.PORT ?? 2000;
@@ -11,10 +13,12 @@ const MONGODB_URI =
   process.env.MONGODB_URI ?? "mongodb://localhost:27017/listings";
 
 // Error / Disconnection
-mongoose.connection.on("error", (err) =>
-  console.log(err.message + " is Mongod not running?")
-);
-mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+mongoose.connection
+  .on("error", (err) => console.log(err.message + " is Mongod not running?"))
+  .on("disconnected", () => console.log("mongo disconnected"))
+  .once("open", () => {
+    console.log("connected to mongoose...");
+  });
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -23,19 +27,18 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
 
-app.use(
-  cors(
-  {
-    credentials:true,
-  origin:true,
-  methods:'GET,HEAD,PUT,PATCH,POST,DELETE'
-  }
+app
+  .use(morgan(morganFmt))
+  .use(
+    cors({
+      credentials: true,
+      origin: true,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
 )
-);
-
-app.use(express.json());
-app.use("/api/listings", ListingController);
-app.use("/auth", AuthController);
+  .use(express.json())
+  .use("/api/listings", ListingController)
+  .use("/auth", AuthController);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Reallistic");
