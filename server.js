ffb4a9f9655151b9be125fb0acc019aafd749f17
Mork <mawwwrk@gmail.com>
2022-04-21 18:38:45 +0800

    auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const ListingController = require("./controllers/listingController");
 const AuthController = require("./controllers/auth");
@@ -34,6 +35,7 @@ app
     })
   )
   .use(express.json())
+  .use(cookieParser())
   .use("/api/listings", ListingController)
   .use("/auth", AuthController);
 
