8c21c25c75393511ef25fbff41c06b45c4ee9b24
Mork <mawwwrk@gmail.com>
2022-04-23 14:07:22 +0800

    commit js files

--- server.js
+++ server.js
@@ -2,30 +2,33 @@
 /* eslint-disable no-undef */
 require("dotenv").config();
 const express = require("express");
+const debug = require("debug");
+const debugserver = debug("app:server:debug");
+const logserver = debug("app:server:log");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const ListingController = require("./controllers/listingController");
-const AuthController = require("./controllers/auth");3
+const AuthController = require("./controllers/auth");
 const tenantUserController = require("./controllers/tenantUserController");
 const morgan = require("morgan");
-const { isProduction, mongoUri, prodUrls, ports } = require("./util/envhelper");
-const morganFmt = isProduction ? "tiny" : "dev";
-const frontEndUrls = [
-  `http://localhost:${ports.local.frontend}`,
-  prodUrls.frontend,
-];
+const { frontEndUrls, mongoUri, prodUrls, ports } = require("./util/envhelper");
+const { User } = require("./models/Users");
+const Listing = require("./models/Listing");
 
 const app = express();
-const PORT = ports.local.backend;
+const PORT = ports.backend;
 const MONGODB_URI = mongoUri;
 
 // Error / Disconnection
 mongoose.connection
-  .on("error", (err) => console.log(err.message + " is Mongod not running?"))
-  .on("disconnected", () => console.log("mongo disconnected"))
+  .on("error", (err) => logserver(err.message + " is Mongod not running?"))
+  .on("disconnecting", () => logserver("mongod disconnected"))
+  .on("disconnected", () => logserver("mongo disconnected"))
   .once("open", () => {
-    console.log("connected to mongoose...");
+    logserver(
+      "connected to mongoose at \033[94,1m" + `${MONGODB_URI}` + "\033[0m"
+    );
   });
 
 mongoose.connect(MONGODB_URI, {
@@ -33,7 +36,7 @@ mongoose.connect(MONGODB_URI, {
 });
 
 app
-  .use(morgan(morganFmt))
+  .use(morgan("tiny"))
   .set("trust proxy", true)
   .use(
     cors({
@@ -43,16 +46,32 @@ app
     })
   )
   .use(express.json())
-  .use(cookieParser())
+  .use(cookieParser("secret"))
   .use("/api/listings", ListingController)
   .use("/auth", AuthController);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Reallistic");
+app.get("/", async (req, res) => {
+  const db = await mongoose.connection.asPromise();
+  debugserver(db.host);
+  debugserver(req);
+  res.status(200).json({
+    message: "Welcome to the backend",
+    server: {
+      mongoInstance: mongoUri,
+      db: db.name,
+    },
+  });
 });
+// const users = await User.find().exec();
+// const listings = await Listing.find().exec();
+// .then((res) => console.log("res", res))
+// .catch((err) => console.log("err", err));
+
+// res.send({ users, listings });
+// res.send("Welcome to Reallistic");
 
 app.use("/api/tenant", tenantUserController);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logserver(`Server is running on port ${PORT}`);
 });
--- util/authentication.js
+++ util/authentication.js
@@ -1,41 +1,52 @@
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const { accessSecret, refreshSecret } = require("./envhelper");
-
-// const hash = ({}) => crypto.createHash("sha256").update("password").digest("hex");
-
-function generateAccessToken(payload) {
-  return jwt.sign(payload, accessSecret, {
-    expiresIn: "15m",
-  });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.verifyRefreshToken = exports.generateRefreshToken = exports.generateAccessToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const envhelper_1 = require("./envhelper");
+const crypto_1 = __importDefault(require("crypto"));
+const expiry = {
+    access: "15m",
+    refresh: "1h",
+};
+const jwtoptions = {
+    encoding: "utf8",
+    algorithm: "HS384",
+    mutatePayload: true,
+    subject: "user",
+    noTimestamp: true,
+};
+const generateAccessToken = (userdata) => jsonwebtoken_1.default.sign(userdata, envhelper_1.accessSecret, { ...jwtoptions, expiresIn: expiry.access });
+exports.generateAccessToken = generateAccessToken;
+const sha256hash = (input) => crypto_1.default.createHash("sha256").update(input).digest("base64");
+function generateRefreshToken(userdata) {
+    const fgp = crypto_1.default.randomBytes(64).toString("base64");
+    const cookieValue = sha256hash(fgp);
+    const refreshToken = jsonwebtoken_1.default.sign({ ...userdata, fgp }, envhelper_1.refreshSecret, {
+        ...jwtoptions,
+        expiresIn: expiry.refresh,
+    });
+    return [refreshToken, cookieValue];
 }
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (token == undefined) return res.sendStatus(401);
-
-  jwt.verify(token, accessSecret, (err, user) => {
-    console.log(err);
-    if (err) return res.sendStatus(403);
-    req.user = user;
+exports.generateRefreshToken = generateRefreshToken;
+const verifyRefreshToken = (token, fgp) => {
+    const decoded = jsonwebtoken_1.default.verify(token, envhelper_1.refreshSecret);
+    if (!decoded)
+        return false;
+    const tfgp = decoded.fgp;
+    return sha256hash(tfgp) === fgp ? decoded : false;
+};
+exports.verifyRefreshToken = verifyRefreshToken;
+const authMiddleWare = (req, res, next) => {
+    const cookie = req.cookies;
+    const headers = req.headers;
+    if (!cookie)
+        return next();
+    const decoded = verifyRefreshToken(cookie, req.cookies.fgp);
+    if (!decoded)
+        return next();
+    req.user = decoded;
     next();
-  });
-}
-
-function generateRefreshToken() {
-  const buf = crypto.randomBytes(64);
-  const fgp = buf.toString("hex");
-
-  const refreshToken = jwt.sign({ fgp }, refreshSecret, {
-    expiresIn: "1h",
-  });
-  return [refreshToken, fgp];
-}
-
-module.exports = {
-  generateAccessToken,
-  authenticateToken,
-  generateRefreshToken,
 };
