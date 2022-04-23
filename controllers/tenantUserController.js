08503c62e1c5aa11089adc0fe1dc3188052c4631
Du Guowei <duguowei1000@gmail.com>
2022-04-23 15:10:40 +0800

    refactor tenantcontroller

--- controllers/tenantUserController.js
+++ controllers/tenantUserController.js
@@ -13,29 +13,6 @@ const router = express.Router();
 //     });
 // });
 
-
-// //* Delete Route
-// router.delete("/:id", async (req, res) => {
-//   await Listing.findByIdAndRemove(req.params.id);
-//   res.json({ message: "Listing Deleted" });
-// });
-
-// router.get('/:id', (req, res) => {
-//   Listing.find({_id: req.params.id})
-//     .then(listing => {
-//       res.json(listing)
-//     })
-//     .catch(err => {
-//       res.json(err)
-//     })
-// })
-
-// //*Put route
-// router.put("/:id", async (req, res) => {
-//   await Listing.findByIdAndUpdate({_id:req.params.id}, req.body);
-//   res.json({ message: "Listing Updated" });
-// });
-
 ////Seed Tenant Listings
 router.get("/seed", async (req, res) => {
 
@@ -84,61 +61,37 @@ router.post("/", async (req, res) => {
   }
 });
 
-//*Put route
-// router.put("/:id", async (req, res) => {
-//   await TenantWatchList.findByIdAndUpdate({_id:req.params.id}, req.body);
-//   res.json({ message: "Listing Updated" });
-// });
-
-
 router.put("/:id", async (req, res) => {
-  console.log('sent')
   const  {fav } = req.body;
   //_id shld be tagged to the logged in User
   const tenantID = req.params.id//"6262c905f7d19a73f07ede29"
-  //console.log("ID>>>",req.params.id)
   const update = await TenantUser.findByIdAndUpdate({_id:tenantID},{$addToSet: {favourites:fav}})
 
   if(update ===null){
     console.log('mongo search is null')
-  }//else //update.favourites =fav
+  }
   //res.send('Added to list')
   res.json({ message: "Added to list" });
-  //console.log('update',update)
+
   //await update.save()
 })
 
 
-// router.delete("/:id", async (req, res) => {
-//   const tenantID = req.params.id//"6262c905f7d19a73f07ede29"
-//   await await TenantUser.findByIdAndUpdate({_id:tenantID},{ $pull: {favourites:deleteFav}})
-  
-//   res.json({ message: "Holiday Deleted" });
-// });
-
-
+//Delete specific listing from favourites
 router.put("/watchlist/:id", async (req, res) => {
   const  {fav } = req.body;
-  console.log('deleteBody' , fav)
+  //console.log('deleteBody' , fav)
   //_id shld be tagged to the logged in User
   const tenantID = req.params.id//"6262c905f7d19a73f07ede29"
-  // const update = await TenantUser.findByIdAndUpdate({_id:tenantID},{$push: {favourites:fav}})
   const update = await TenantUser.findByIdAndUpdate({_id:tenantID},{ $pull: {favourites:fav}})
-  //.then((listings) => {
-    // res.json(update);
-  //})
+
 
   if(update ===null){
     console.log('mongo search is null----pull')
-  }//else //update.favourites =fav
+  }
   
-  //console.log('update--pull',update)
-  //await update.save()
-  // res.send('Deleted from list')
   res.json({ message: "Deleted from list" });
 
 })
-
-
 
 module.exports = router;
